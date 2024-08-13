22410ffd6640121fd48bd8e5d8e7e614e5bfa0fd
Marlon Ferreira <ferreira.marlon@live.com>
2024-08-13 20:34:48 -0300

    feat: Yugioh API request and rendering of cards on the Home Page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,45 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Header from "./_header";
+import Image from "next/image";
+
+interface Card {
+    id: number;
+    name: string;
+    card_images: {
+        image_url: string;
+    }[];
+}
 
 const Home = () => {
-    return <Header />;
+    const [cards, setCards] = useState<Card[]>([]);
+
+    useEffect(() => {
+        const fecthCards = async () => {
+            try {
+                const response = await fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php/");
+                const data = await response.json();
+                setCards(data.data);
+            } catch (error) {
+                console.log(`Error ao buscar dados do card: ${error}`);
+            }
+        };
+
+        fecthCards();
+    }, []);
+
+    return (
+        <>
+            <Header />
+
+            <div className="flex flex-wrap items-center justify-center gap-5 p-5">
+                {cards.map((card) => (
+                    <Image className="duration-500 hover:scale-105 cursor-pointer" key={card.id} src={card.card_images[0].image_url} alt={card.name} width={350} height={350} />
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default Home;
