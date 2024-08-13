9964d324e9cccd3bd2a04e1dfde5bcbc0e3d6d16
Marlon Ferreira <ferreira.marlon@live.com>
2024-08-13 20:33:34 -0300

    feat: added Header component

--- app/(home)/_header.tsx
+++ app/(home)/_header.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
+
 const Header = () => {
     return (
-        <header className="flex items-center justify-around border-b-white border-b h-16 md:h-20">
-            <img src="/logo.png" alt="Yu-Gi-Oh!" className="h-14 md:h-20" />
+        <header className="flex items-center justify-around border-b-white border-b h-20">
+            <div className="relative hidden md:block md:h-[80px] md:w-[160px] h-[50px] w-[100px]">
+                <Image src="/logo.png" alt="Yu-Gi-Oh!" fill />
+            </div>
             <input className="rounded-md max-w-[10rem] focus:outline-none placeholder:text-sm py-1 px-2 text-sm md:text-lg md:max-w-[15rem] " type="text" placeholder="Buscar por nome" />
         </header>
     );
